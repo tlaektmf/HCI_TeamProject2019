45f2931ea24055866ecfd5f5d47611fd3c8ef143
tlaektmf <dazzel0826@gmail.com>
2019-04-02 18:40:43 +0900

    minigame 1 오류 수정, 리소스 추가
    minigame 3에 사용될 리소스 추가

---
+++ HCITeamProject/Assets/Scripts/mini3_PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mini3_PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    //플레이어가 이동을 하지는 않고, 오브젝트들의 사이즈가 바뀜
+
+        //}
+       
+    }
+}
