5f1445f1cf5fd7795618e7c639f49918d82c8b17
tlaektmf <dazzel0826@gmail.com>
2019-04-02 20:20:32 +0900

    miniGame 3 구성완료
    - 리소스 변경 작업 추후 진행예정

--- HCITeamProject/Assets/Scripts/mini3_PlayerController.cs
+++ HCITeamProject/Assets/Scripts/mini3_PlayerController.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class mini3_PlayerController : MonoBehaviour
 {
+    //오브젝트 사이즈 변경
+    GameObject mevil;
+    GameObject mcastle;
+    GameObject mroad;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.mevil = GameObject.Find("evil_emotion0");
+        this.mcastle = GameObject.Find("castle");
+        this.mroad = GameObject.Find("road");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.UpArrow))
-        //{
-        //    //플레이어가 이동을 하지는 않고, 오브젝트들의 사이즈가 바뀜
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            //플레이어가 이동을 하지는 않고, 오브젝트들의 사이즈가 바뀜
+            //transform.localScale += new Vector3(0.5f, 0.5f, 0);
+            UpButton();
+        }
 
-        //}
-       
+    }
+
+    /*
+ 사용자 정의 함수
+ */
+    public void UpButton()
+    {
+        mevil.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+        mcastle.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+        mroad.transform.Translate(0, -0.1f, 0);
     }
 }
