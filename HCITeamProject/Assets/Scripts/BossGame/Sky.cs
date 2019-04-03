3fa06f53794718b1296dccac1960de554422d8cb
yoeri1123 <yoeri1123@naver.com>
2019-04-03 14:08:34 +0900

    [김여리][원래 따로 하고있었던 게임에서 브런치로 자료 옮김.]

---
+++ HCITeamProject/Assets/Scripts/BossGame/Sky.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sky : MonoBehaviour {
+
+    Material mat;           // 머티리얼 
+    float speed = 0.01f;    // 스크롤 속도
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 ofs = mat.mainTextureOffset;
+        ofs.y += speed * Time.deltaTime;
+
+        mat.mainTextureOffset = ofs;
+    }
+}
+
