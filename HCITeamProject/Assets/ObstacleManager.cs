f43f7930d5a01f31c35fe7462a0e323434342a62
lhwanstar <lhwanstar@gmail.com>
2019-04-03 20:24:22 +0900

    이미지 작업 컨트롤러 작업 장애물 매니저 작업

---
+++ HCITeamProject/Assets/ObstacleManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Obstacle
+{
+    public float time;
+    public int type;
+    public Obstacle(float time, int type)
+    {
+        this.time = time;
+        this.type = type;
+    }
+}
+
+public class ObstacleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject enemyPrefab = null;
+    GameObject om = null;
+    public Obstacle nextObstacle = null;
+    public List<Obstacle> obstacleList = new List<Obstacle>();
+
+    int cnt = 0;
+    public void genEnemy(int type)
+    {
+        Debug.Log(enemyPrefab);
+        if(enemyPrefab)
+        {
+            GameObject enemy = MonoBehaviour.Instantiate(enemyPrefab) as GameObject;
+            enemy.transform.parent = om.transform;
+            enemy.name = "obs" + cnt++;
+        }
+    }
+
+    void Start()
+    {
+        om = GameObject.Find("ObstacleManager");
+        enemyPrefab = Resources.Load("game2/object/Enemy") as GameObject;
+
+        obstacleList.Add(new Obstacle(5.0f, 1));
+        obstacleList.Add(new Obstacle(8.0f, 1));
+        obstacleList.Add(new Obstacle(11.0f, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obstacleList.Count > 0)
+        {
+            nextObstacle = obstacleList[0];
+            if(nextObstacle.time <= Time.time)
+            {
+                genEnemy(nextObstacle.type);
+                obstacleList.Remove(nextObstacle);
+                if (obstacleList.Count == 0) nextObstacle = null;
+                else nextObstacle = obstacleList[0];
+            }
+        }
+    }
+}
---
+++ HCITeamProject/Assets/Scripts/game2/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 move;
+    void Start()
+    {
+        move = new Vector3(-1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + move * Game2Controller.speed * Time.deltaTime;
+    }
+}
