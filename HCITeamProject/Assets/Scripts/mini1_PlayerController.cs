3b00cf40642fe3b7c7b4c3d0b33a9071ff6b70a2
tlaektmf <dazzel0826@gmail.com>
2019-04-03 13:57:11 +0900

    Update mini1_PlayerController.cs

--- HCITeamProject/Assets/Scripts/mini1_PlayerController.cs
+++ HCITeamProject/Assets/Scripts/mini1_PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class mini1_PlayerController : MonoBehaviour
 {
+    GameObject mbackground;
     // Start is called before the first frame update
     void Start()
-    {
-
+    {  
+        this.mbackground = GameObject.Find("minigame1_background");
     }
 
     // Update is called once per frame
@@ -16,10 +17,12 @@ public class mini1_PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.Translate(-1.1f, 0, 0);//왼쪽으로 3만큼 이동
+            mbackground.transform.Translate(0.3f, 0, 0);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.Translate(1.1f, 0, 0);//오른쪽으로 3만큼 이동
+            mbackground.transform.Translate(-0.3f, 0, 0);
         }
     }
 
@@ -34,4 +37,5 @@ public class mini1_PlayerController : MonoBehaviour
     {
         transform.Translate(1.1f, 0, 0);
     }
+    
 }
