76d7525390443fd7e4c41c7dd14e68df89729d1e
tlaektmf <dazzel0826@gmail.com>
2019-04-05 22:57:33 +0900

    Revert "폭탄 랜덤생성, 화면 제어"
    This reverts commit 757f5173add36e5abaef96daee369fe7a166c7ab.

--- HCITeamProject/Assets/Scripts/BombGenerator.cs
+++ HCITeamProject/Assets/Scripts/BombGenerator.cs
@@ -4,50 +4,26 @@ using UnityEngine;
 
 public class BombGenerator : MonoBehaviour
 {
-    int BOUND_LEFT = -2;
-    int BOUNT_RIGHT = 2;
-    bool isFinish = true;
-
     public GameObject mbombPrefab;//폭탄 프리팹을 넣을 변수(아울렛)
-    float span;//1초마다 1개씩 폭탄을 생성
+    float span=1.0f;//1초마다 1개씩 폭탄을 생성
     float delta = 0;
     // Start is called before the first frame update
     void Start()
     {
-        span = 1.0f;//1초마다 1개씩 폭탄을 생성(초기설정)
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
         this.delta += Time.deltaTime;//Time.deltaTime : 앞프레임과 현재 프레임의 시간차이
-
-        if (isFinish == false)
+        if (this.delta > this.span)
         {
-            span= setRandomSpan();
-            isFinish = true;
-            Debug.Log("set new span: "+span);
+            this.delta = 0;
+            GameObject obj = Instantiate(mbombPrefab) as GameObject;
+            int appera_x = Random.Range(-6, 7);//-6 ~ 7 범위로 지정
+            obj.transform.position = new Vector3(appera_x, 7, 0);
         }
-        else
-        {
-            if (this.delta > this.span)
-            {
-                this.delta = 0;
-                GameObject obj = Instantiate(mbombPrefab) as GameObject;
-                int appera_x = Random.Range(BOUND_LEFT, BOUNT_RIGHT);//x좌표 범위 지정
-                obj.transform.position = new Vector3(appera_x, 7, 0);
-                isFinish = false;
-            }
-        }
-       
-        
-        
-    }
-
-    float setRandomSpan()//폭탄이 떨어지는 속도 설정
-    {
-        return Random.Range(0.5f, 1);//0.5초~1초
         
     }
 }
--- HCITeamProject/Assets/Scripts/mini1_PlayerController.cs
+++ HCITeamProject/Assets/Scripts/mini1_PlayerController.cs
@@ -5,33 +5,21 @@ using UnityEngine;
 public class mini1_PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    int BOUND_LEFT = -3;
-    int BOUNT_RIGHT = 3;
-    GameObject mbackground;
     void Start()
     {
-        this.mbackground = GameObject.Find("minigame1_background");//케릭터오브젝트
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 worldpos = Camera.main.WorldToViewportPoint(this.transform.position);
-        if (worldpos.x < 0f) worldpos.x = 0.1f;
-        if (worldpos.y < 0f) worldpos.y = 0f;
-        if (worldpos.x > 1f) worldpos.x = 0.9f;
-        if (worldpos.y > 1f) worldpos.y = 1f;
-        this.transform.position = Camera.main.ViewportToWorldPoint(worldpos);
-
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.Translate(-1.1f, 0, 0);//왼쪽으로 3만큼 이동
-            mbackground.transform.Translate(0.1f, 0, 0);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.Translate(1.1f, 0, 0);//오른쪽으로 3만큼 이동
-            mbackground.transform.Translate(-0.1f, 0, 0);
         }
     }
 
