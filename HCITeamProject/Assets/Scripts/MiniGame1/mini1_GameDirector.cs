2238c64d09ea334e51fc162710819763291ae3e5
tlaektmf <dazzel0826@gmail.com>
2019-04-06 17:14:08 +0900

    오류수정

--- HCITeamProject/Assets/Scripts/MiniGame1/mini1_GameDirector.cs
+++ HCITeamProject/Assets/Scripts/MiniGame1/mini1_GameDirector.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameDirector : MonoBehaviour
+public class mini1_GameDirector : MonoBehaviour
 {
-    int CLEAR_CONTROL_TIME =3;
-    int CASTLE_SHOW_CONTROL_TIME = 3;
+    int CLEAR_CONTROL_TIME = 10;
+
     int PLAYER_SIZE_CONTROL_TIME = 5;
 
     GameObject mplayer;
@@ -15,29 +15,29 @@ public class GameDirector : MonoBehaviour
     void Start()
     {
         this.mplayer = GameObject.Find("player_man");
-       /// this.mevil = GameObject.Find("evil");
+        /// this.mevil = GameObject.Find("evil");
         StartCoroutine(miniGame1Clear());
-        StartCoroutine(miniGame3End());
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     /*
      사용자 정의 함수 - 미니게임 1
      */
 
-     
+
     public void eatMashroom()
     {
         //mushroom과 충돌하는 경우
         Debug.Log("eat mashroom");
 
         StartCoroutine(changeSize());
-        
+
     }
     public IEnumerator changeSize()
     {
@@ -53,30 +53,6 @@ public class GameDirector : MonoBehaviour
         Debug.Log("mini game 1 clear");
         SceneManager.LoadScene("ClearMiniGame1");
     }
-
-
-    /*
-     사용자 정의 함수 - 미니게임 3
-     */
-
-    public IEnumerator miniGame3Clear()
-    {
-        //evil과 충돌하는 경우 clear
-
-        //1. 우선 evil을 없애고
-        //2. 3초후, 클리어 화면으로 전환한다
-
-        yield return new WaitForSeconds(CASTLE_SHOW_CONTROL_TIME);//WaitForSeconds객체를 생성해서 반환
-        SceneManager.LoadScene("ClearMiniGame3");
-        Debug.Log("mini game 3 clear");
-    }
-
-    public IEnumerator miniGame3End()
-    {
-        yield return new WaitForSeconds(CLEAR_CONTROL_TIME);//WaitForSeconds객체를 생성해서 반환
-        GameOver();//40초가 지나면 game over
-    }
-
     /*
      * 사용자 정의 함수- 미니게임 1, 미니게임 3 공통
      */
@@ -87,6 +63,5 @@ public class GameDirector : MonoBehaviour
         Debug.Log("game over , show bad ending");
         SceneManager.LoadScene("BadEnding");
     }
-
 
 }
