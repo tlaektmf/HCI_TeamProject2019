99ba5c695ee1265857ac022490f05ba6f06b2cde
tlaektmf <dazzel0826@gmail.com>
2019-04-10 11:44:19 +0900

    testscnece

---
+++ HCITeamProject/Assets/Scripts/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(demoThread());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator demoThread()
+    {
+        yield return StartCoroutine(checkFile());
+    }
+    
+    //public IEnumerator checkFile()
+    //{
+
+    //}
+}
