ab3bfa01a42dd3cf979f4b302c5bb60f497a6552
lhwanstar <lhwanstar@gmail.com>
2019-04-28 22:23:31 +0900

    resoultion Fix code at Mapview

--- HCITeamProject/Assets/MapViewAudio.cs
+++ HCITeamProject/Assets/MapViewAudio.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class MapViewAudio : MonoBehaviour
 {
     // Start is called before the first frame update
+    void Awake()
+    {
+        Screen.SetResolution(720, 1280, true);
+        Camera.main.orthographicSize = 1280 / (100.0f * 2.0f); // 카메라 수직길이 절반 / 100
+    }
     void Start()
     {
         SoundManager.Instance.PlayMusicWithPath("audio/common/mapview");
+
     }
 
     // Update is called once per frame
